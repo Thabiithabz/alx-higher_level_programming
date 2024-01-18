cea8220250e44b75be263c3890ac2327e063114c
Thabiithabz <thabangmahlatsemalahlela@gmail.com>
2024-01-18 05:39:04 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-if __name__ = "__main__":
-    from sys import agrv
-    x = len(agrv) - 1
+if __name__ == "__main__":
+    from sys import argv
+    x = len(argv) - 1
     if x < 1:
         print("{} argument.".format(x))
     elif x == 1:
