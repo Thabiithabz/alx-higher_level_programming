e7dee262f97f383a14a7f6a0e6820b698c5612b6
Thabiithabz <thabangmahlatsemalahlela@gmail.com>
2024-01-18 01:02:19 -0800

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for x in range(1,101):
-        """checkfor multiples of 3 and 5and replace with fizzbuzz"""
-        if x % 3 == 0 and x % 5 == 0:
-            print("{fizzbuzz}", end='')
-            fizzbuzz()
+    for x in range(1, 101):
+        if x % 3 == 0 and x % 5 != 0:
+            print("fizz ", end='')
+        elif x % 5 == 0 and x % 3 != 0:
+            print("Buzz ", end='')
+        elif x % 5 == 0 and x % 3 == 0:
+            print("fizzbuzz ", end='')
+        else:
+            print("{}".format(x), end='')
