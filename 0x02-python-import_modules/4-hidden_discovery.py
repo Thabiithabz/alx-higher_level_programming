9c0384b527a1aa27b1cc86d4e91ddaeaa3527f2a
Thabiithabz <thabangmahlatsemalahlela@gmail.com>
2024-01-18 06:12:09 -0800

    4-hidden_discovery.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from hidden_4 import *
+    allf = dir()
+    for i in range(0, len(allif)):
+        if allif[i][:2] != "_":
+            print("{:s}".format(allif[i]))
