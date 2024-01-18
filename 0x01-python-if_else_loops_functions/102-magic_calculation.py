e3ba895c9be83fcb03c3b0131667b740b44ed8f2
Thabiithabz <thabangmahlatsemalahlela@gmail.com>
2024-01-18 01:40:31 -0800

    102-magic_calculation.py

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -4,4 +4,7 @@ def remove_char_at(str, n):
         return (c)
     elif c > b:
         return a + b 
-    return a * b - c 
+    return a * b - c
+print(magic_calculation.magic_calculation(1, 2, 3))
+print(magic_calculation.magic_calculation(2, 1, 3))
+ print(magic_calculation.magic_calculation(3, 1, 2))
